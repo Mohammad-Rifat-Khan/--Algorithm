665fa9076b175ed300ca5643727d38900e71fb52
Mohammad Rifat Khan <119173093+Mohammad-Rifat-Khan@users.noreply.github.com>
2023-02-03 00:08:56 +0600

    Update GCD.cpp

--- GCD.cpp
+++ GCD.cpp
@@ -1,8 +1,22 @@
+/* This calculation is done by using Euclid's in-built function; 
+if you want to write the function, here it is-> 
+int GCD(int x,int y ){
+    if (y==0)
+    {
+        return x;
+    }
+    else
+    return GCD(y, x%y);
+ }
+
+ for more than two numbers just use __gcd(__gcd(12,18),20);
+
+ */
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    //__gcd(12,18);
 cout<<__gcd(12,18)<<endl;
     return 0;
 }
