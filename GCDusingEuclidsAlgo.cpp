1a2020e70df3f7c8b3f40d937e02b4689d83dd32
Mohammad-Rifat-Khan <22-46060-1@student.aiub.edu>
2023-02-02 22:58:24 +0600

    GCD using Euclid's algorithom

---
+++ GCDusingEuclidsAlgo.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ int GCD(int x,int y ){
+    if (y==0)
+    {
+        return x;
+    }
+    else
+    return GCD(y, x%y);
+ }
+
+ int main(){
+    int a,b;
+    cout<<"Enter the 1st number: ";
+    cin>>a;
+    cout<<"Enter the 2nd number: ";
+    cin>>b;
+
+    cout<<"The greatest common divisor of the input is: "<<GCD(a,b)<<endl;
+
+    return 0;
+ }
